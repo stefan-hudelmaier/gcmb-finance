10b6740e7315770362f1ff6551d24262aece1cae
Stefan Hudelmaier <stefan.hudelmaier@device-insight.com>
2025-05-09 23:52:41 +0200

    Use gcmb-publisher package

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-import json
 import yliveticker
-import paho.mqtt.client as mqtt
 import os
 import logging
 import sys
 from symbols import all_symbols, all_symbols_by_identifier
 import time
+from gcmb_publisher import MqttPublisher
 from threading import Thread
-
-broker = os.environ.get('MQTT_HOST', 'gcmb.io')
-client_id = os.environ['MQTT_CLIENT_ID']
-username = os.environ['MQTT_USERNAME']
-password = os.environ['MQTT_PASSWORD']
-port = 8883
 
 log_level = os.environ.get('LOG_LEVEL', 'INFO')
 print("Using log level", log_level)
@@ -29,41 +22,7 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-last_successful_message = None
-started = time.time()
-
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc, properties):
-        if rc == 0:
-            logger.info("Connected to MQTT Broker")
-        else:
-            logger.error(f"Failed to connect, return code {rc}")
-
-    def on_disconnect(client, userdata, disconnect_flags, reason_code, properties):
-        logger.warning(f"Disconnected from MQTT Broker, return code {reason_code}")
-
-    mqtt_client = mqtt.Client(client_id=client_id, callback_api_version=mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
-    mqtt_client.tls_set(ca_certs='/etc/ssl/certs/ca-certificates.crt')
-    mqtt_client.username_pw_set(username, password)
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_disconnect = on_disconnect
-
-    # Connection will be made once loop_start() has been called
-    mqtt_client.connect_async(broker, port)
-    return mqtt_client
-
-
-def mqtt_publish(client, topic, msg):
-    result = client.publish(topic, msg, retain=True)
-    status = result.rc
-    if status == 0:
-        logger.debug(f"Sent '{msg}' to topic {topic}")
-        return True
-    else:
-        logger.warning(f"Failed to send message to topic {topic}, reason: {status}")
-        return False
-
+mqtt_publisher = MqttPublisher(enable_watchdog=True)
 
 def on_websocket_close(ws, close_status, close_msg):
     logger.error(f"Websocket closed: {close_status} {close_msg}. Exiting.")
@@ -77,22 +36,7 @@ def on_websocket_error(ws, error):
     sys.exit(1)
 
 
-def watchdog():
-    while True:
-        time.sleep(60)
-        logger.info(f"Watchdog checking for inactivity: Last successful message was {last_successful_message}")
-        now = time.time()
-        if last_successful_message is not None and now - last_successful_message > 10 * 60:
-            logger.error("No messages sent in the ten minutes, restarting")
-            # sys.exit would not work in a thread
-            os._exit(1)
-        elif last_successful_message is None and now - started > 5 * 60:
-            logger.error("No message has been sent yet and service is already running for five minutes, restarting")
-            os._exit(1)
-
-
 def main():
-    mqtt_client = connect_mqtt()
 
     def on_new_msg(ws, update):
         try:
@@ -102,19 +46,11 @@ def main():
                 return
             symbol = all_symbols_by_identifier[symbol_id]
             price = update['price']
-            successful_publish = mqtt_publish(mqtt_client, symbol.topic(), price)
+            mqtt_publisher.send_msg(price, symbol.topic(), retain=True)
 
-            if successful_publish:
-                global last_successful_message
-                last_successful_message = time.time()
         except Exception as e:
             logging.error(f"Error processing message: {e}")
 
-
-    mqtt_client.loop_start()
-
-    watchdog_thread = Thread(target=watchdog, args=())
-    watchdog_thread.start()
 
     yliveticker.YLiveTicker(
         ticker_names=[s.yahoo_identifier for s in all_symbols],
